b2bb71d176e00c41378891b5b70b74b8c604a7c4
JuanL1945 <jlaquisc@fcpn.edu.bo>
2024-01-15 11:47:13 -0600

    contador-react

--- semana2/mi-primera-app/src/components/EventoB.js
+++ semana2/mi-primera-app/src/components/EventoB.js
@@ -3,8 +3,7 @@ import "../styles/Evento.css";
 function EventoB(props) {
     return (
         <div className="card-container">
-            <img className="card-image">
-            </img>
+            <img className="card-image" src={"https://pylapaz.org/media/images/pypizza03.2e16d0ba.fill-256x256.format-webp-lossless.webp"}></img>
             <div className="card-content">
                 <h2 className="card-title">{props.title}</h2>
                 <p className="card-date">{props.dateLiteral}</p>
---
+++ semana3/mis-eventos-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import { useState } from "react";
+
+function App() {
+  const [nroClicks, setNumClicks] = useState(0);
+  const click = () => {
+    setNumClicks(nroClicks + 1);
+    console.log("click");
+
+  }
+  const reiniciar = () => {
+    setNumClicks(0);
+    console.log("reiniciar");
+  }
+  return (
+    <div className="App">
+      <div className="contenedor-principal">
+        <Contador nroClicks={nroClicks} />
+        <div className='contendedor-botones'>
+          <Boton texto="Click" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

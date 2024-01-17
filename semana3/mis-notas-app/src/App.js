36bea44a192a59a1d0e69ba8f82a54cf7ed25be8
JuanL1945 <jlaquisc@fcpn.edu.bo>
2024-01-17 11:58:50 -0600

    notas

---
+++ semana3/mis-notas-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListaNotas from './components/ListaNotas';
+import Nota from './components/Nota';
+function App() {
+  return (
+    <div className="App">
+      <ListaNotas />
+    </div>
+  );
+}
+
+export default App;

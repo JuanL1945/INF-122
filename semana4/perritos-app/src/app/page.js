8cde0dbe23dedd78abbd680be967aad0c5d4c2cb
JuanL1945 <jlaquisc@fcpn.edu.bo>
2024-01-24 11:30:26 -0600

    perritos

---
+++ semana4/perritos-app/src/app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+import Dog from "./components/dogs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>PERRITOS</h1>
+      <Dog />
+    </div>
+  );
+}
